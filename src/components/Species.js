834195169e99b883298ce3d4b9e1c051edf12566
Dallin Parker <dallin.r.parker@gmail.com>
2017-07-05 13:05:57 -0600

    working with function Species component

--- src/components/Species.js
+++ src/components/Species.js
@@ -1,41 +1,42 @@
 import React, { Component } from 'react'
 
-class Species extends Component {
-
-	handleClick(){
-		this.props.action(this.props.name)
-	}
-
-	render() {
-		return (
-			<div style={{border: '2px lightblue solid'}}>
-				<h5>{this.props.name}</h5>
-				<p style={
-					{ color: 'blue',
-						fontFamily: 'Courier'
-					}
-				}
-				>{this.props.status}</p>
-				<button onClick={this.handleClick.bind(this)}
-								style={{color: 'red', marginBottom: '15px'}}>FEED</button>
-			</div>
-		)
-	}
-}
-
-export default Species
-
-// THIS IS A VIEW COMPONENT
-//export default function Species(props){
-//	function handleClick(props){
-//		props.action(props.name)
+//class Species extends Component {
+//
+//	handleClick(){
+//		this.props.action(this.props.name)
 //	}
 //
-//	return(
-//		<div>
-//			<h5>{props.name}</h5>
-//			<p>{props.status}</p>
-//			<button onClick={handleClick}>FEED</button>
-//		</div>
-//	)
+//	render() {
+//		return (
+//			<div style={{border: '2px lightblue solid'}}>
+//				<h5>{this.props.name}</h5>
+//				<p style={
+//					{ color: 'blue',
+//						fontFamily: 'Courier'
+//					}
+//				}
+//				>{this.props.status}</p>
+//				<button onClick={this.handleClick.bind(this)}
+//								style={{color: 'red', marginBottom: '15px'}}>FEED</button>
+//			</div>
+//		)
+//	}
 //}
+//
+//export default Species
+
+// THIS IS A VIEW COMPONENT
+export default function Species(props){
+
+	function handleClick(){
+		props.action(props.name)
+	}
+
+	return(
+		<div>
+			<h5>{props.name}</h5>
+			<p>{props.status}</p>
+			<button onClick={handleClick}>FEED</button>
+		</div>
+	)
+}
